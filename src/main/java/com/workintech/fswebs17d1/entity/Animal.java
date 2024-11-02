1d6c88fa9bf39adae068a1534dae26d04f15ba25
ElifAkan <elif.akan.2002@hotmail.com>
2024-11-02 22:00:34 +0300

    s17d1

---
+++ src/main/java/com/workintech/fswebs17d1/entity/Animal.java
@@ -0,0 +1,33 @@
+package com.workintech.fswebs17d1.entity;
+
+public class Animal {
+    private Integer id;
+    private String name;
+    public Animal(Integer id,String name){
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Animal{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
